6fb87b05de27860cbd7c669f60e46590c8fbb38d
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-23 21:48:12 -0500

    configuracion de enviroment

--- README.md
+++ README.md
@@ -71,3 +71,10 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+## Iniciar proyecto
+```
+docker compose up -d
+```
+
+copiar el ``` .env.template ``` y renombrarlo a ```.env```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot(''),
+    AuthModule],
   controllers: [],
   providers: [],
 })
